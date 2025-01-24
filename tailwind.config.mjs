d329b4c95db0098ebda73300f4d26c8558ccf39c
sam <aquaticcalf@proton.me>
2025-01-24 12:02:27 +0530

    navigation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    safelist: [
+        'bg-red-400',
+        'bg-orange-400',
+        'bg-sky-400',
+        'bg-green-400',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
